9e0a7d21d7dea8129ff383cf474ded9ec69cd0c4
alex <alex@pcshards.com>
2021-02-16 15:58:48 -0500

    Added Tables to PolicyBoard

--- src/app/policy-board/policy-board.component.ts
+++ src/app/policy-board/policy-board.component.ts
@@ -1,7 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+//delete this trash later
+export interface PeriodicElement {
+  Subtitle: string;
+  Title: string;
+  Status: string;
+}
 
-
+const ELEMENT_DATA: PeriodicElement[] = [
+  {Title: "AC-N.01", Subtitle: 'Account Management', Status: "Implemented"},
+  {Title: "AC-N.02", Subtitle: 'Access Enforcement', Status: "Implemented", },
+  {Title: "AC-N.03", Subtitle: 'Use of External Information Systems', Status: "Implemented"},
+  {Title: "AC-N.04", Subtitle: 'Publicly Accessible Content', Status: "Implemented"},
+  {Title: "AC-N.05", Subtitle: 'System Use Notification', Status: "Not Implemented"},
+  {Title: "AC-N.06", Subtitle: 'Account Management', Status: "Implemented"},
+  {Title: "AC-N.07", Subtitle: 'Access Enforcement', Status: "Implemented", },
+  {Title: "AC-N.08", Subtitle: 'Use of External Information Systems', Status: "Implemented"},
+  {Title: "AC-N.09", Subtitle: 'Publicly Accessible Content', Status: "Implemented"},
+  {Title: "AC-N.10", Subtitle: 'System Use Notification', Status: "Not Implemented"},
+  {Title: "AC-N.11", Subtitle: 'Account Management', Status: "Implemented"},
+  {Title: "AC-N.12", Subtitle: 'Access Enforcement', Status: "Implemented", },
+  {Title: "AC-N.13", Subtitle: 'Use of External Information Systems', Status: "Implemented"},
+  {Title: "AC-N.14", Subtitle: 'Publicly Accessible Content', Status: "Implemented"},
+  {Title: "AC-N.15", Subtitle: 'System Use Notification', Status: "Not Implemented"},
+];
 
 
 
@@ -14,10 +36,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PolicyBoardComponent implements OnInit {
   panelOpenState = false;
-
+  displayedColumns: string[] = ['Title', 'Subtitle', 'Status'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void{
+
+  }
+  onRowClicked(row): void {
+    console.log("Row clicked: ", row);
   }
 
 }
+
+//to setup a service between backend and table
+// or implementing custom CDK data source
+// 
+//https://blog.angular-university.io/angular-material-data-table/
